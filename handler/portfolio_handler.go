4cbd2ed56ab56c5eea751d370a2f131ad9f01fb5
Mounika <mounikavari9@gmail.com.com>
2026-07-01 17:09:12 -0500

    Add portfolio summary handler

--- handler/portfolio_handler.go
+++ handler/portfolio_handler.go
@@ -1,1 +1,38 @@
 package handler
+
+import(
+	"encoding/json"
+	"net/http"
+	""
+)
+
+type PortfolioSummary struct {
+	UserID 	string 		`json:"user_id"`
+	Role 	string		`json:"role"`
+	TotalCash	float64 	`json:"total_cash"`
+	Message 	string 		`json:"message"`
+}
+
+//this function is called only after JWT middleware successfully validates the user's token
+func GetPortfolioSummary(w http.ResponseWriter, r *http.Request){
+	//Read UserID from request context
+	//JWT middleware stored it here after validating the JWT token
+	userID := r.Context().Value(middleware.UserIDKey).(string)
+
+	//read role from the request context
+	role := r.Context().Value(middleware.RoleKey).(string)
+
+	response := PortfolioSummary{
+		UserID : userID,
+		Role: role,
+		TotalCash: 2500.50,
+		Message: "Portfolio summary accessed successfully",
+	}
+
+	//tell client that response formate is JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	//convert response struct into JSON and send back to the client
+	json.NewEncoder(w).Encode(response)
+
+}
